08de77353be78739d1640b38f47cb512e835bc23
nüke <nuke.twoski@gmail.com>
2021-12-05 18:17:29 -0500

    test

--- scripts/FormulaBody.jsx
+++ scripts/FormulaBody.jsx
@@ -25,16 +25,16 @@ class FormulaBody extends React.Component {
     _renderDataList() {
         let elements = [];
         let count = 0;
-        for (let ingredient in PN.database.materials) {
+        for (let ingredient of PN.database.materials) {
             count = count + 1;
-            elements.push(<option key={"material" + count} label={ingredient.name} value={ingredient.id}/>);
+            elements.push(<option label={ingredient.name} value={ingredient.id}/>);
         }
-        for (let ingredient in PN.database.mixtures) {
+        for (let ingredient of PN.database.mixtures) {
             count = count + 1;
             if (ingredient.diluted_material != null) {
-                elements.push(<option key={"dilution" + count} label={ingredient.name + PN.getDilutionPercentString(ingredient)} value={ingredient.id}/>);
+                elements.push(<option label={ingredient.name + PN.getDilutionPercentString(ingredient)} value={ingredient.id}/>);
             } else {
-                elements.push(<option key={"mixture" + count} label={ingredient.name} value={ingredient.id}/>);
+                elements.push(<option label={ingredient.name} value={ingredient.id}/>);
             }   
         }
         return (
