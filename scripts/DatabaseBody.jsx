5a60bd49ad19a3e4dee521f07afa06f53909ef4b
nüke <nuke.twoski@gmail.com>
2021-12-10 20:53:49 -0500

    database

--- scripts/DatabaseBody.jsx
+++ scripts/DatabaseBody.jsx
@@ -23,33 +23,33 @@ class DatabaseBody extends React.Component {
                         <tr>
                             <td>
                                 ID: 
-                                <input onChange={(event) => this._onChangeMaterial("id", event.target.value.toString().toLower())}/>
+                                <input className="databaseinput" onChange={(event) => this._onChangeMaterial("id", event.target.value.toString().toLower())}/>
                             </td>
                             <td>
                                 NAME: 
-                                <input onChange={(event) => this._onChangeMaterial("name", this._formatName(event.target.value))}/>
+                                <input className="databaseinput" onChange={(event) => this._onChangeMaterial("name", this._formatName(event.target.value))}/>
                             </td>
                             <td>
                                 COMPANY: 
-                                <input onChange={(event) => this._onChangeMaterial("name", this._formatName(event.target.value))}/>
+                                <input className="databaseinput" onChange={(event) => this._onChangeMaterial("name", this._formatName(event.target.value))}/>
                             </td>
                         </tr>
                         <tr>
                             <td colSpan="3">
                                 SCENT: 
-                                <input onChange={(event) => this._onChangeMaterial("scent", this._formatName(event.target.value))}/>
+                                <input className="databaseinput" onChange={(event) => this._onChangeMaterial("scent", this._formatName(event.target.value))}/>
                             </td>
                         </tr>
                         <tr>
                             <td colSpan="3">
                                 USAGE: 
-                                <input onChange={(event) => this._onChangeMaterial("usage", this._formatName(event.target.value))}/>
+                                <input className="databaseinput" onChange={(event) => this._onChangeMaterial("usage", this._formatName(event.target.value))}/>
                             </td>
                         </tr>
                         <tr>
                             <td>
                                 NOTE: 
-                                <select onChange={(event) => this._onChangeMaterial("note", event.target.value.toString().toLower())}>
+                                <select className="databaseinput" onChange={(event) => this._onChangeMaterial("note", event.target.value.toString().toLower())}>
                                     <option value="TOP">TOP</option>
                                     <option value="HEART">HEART</option>
                                     <option value="BASE">BASE</option>
@@ -57,7 +57,7 @@ class DatabaseBody extends React.Component {
                             </td>
                             <td>
                                 LONGEVITY: 
-                                <input onChange={(event) => this._onChangeMaterial("longevity", event.target.value)}/>
+                                <input className="databaseinput" onChange={(event) => this._onChangeMaterial("longevity", event.target.value)}/>
                             </td>
                             <td>
                                 IMPACT: 
@@ -69,7 +69,7 @@ class DatabaseBody extends React.Component {
                         <tr>
                             <td>
                                 RECOMMENDED DILUTION: 
-                                <input onChange={(event) => this._onChangeMaterial("dilute", event.target.value)}/>
+                                <input className="databaseinput" onChange={(event) => this._onChangeMaterial("dilute", event.target.value)}/>
                             </td>
                             <td>
                                 AVERAGE % USED IN CONCENTRATE: 
@@ -89,11 +89,11 @@ class DatabaseBody extends React.Component {
                         <tr>
                             <td>
                                 CAS NUMBER: 
-                                <input onChange={(event) => this._onChangeMaterial("cas", event.target.value.toString().toUpperCase())}/>
+                                <input className="databaseinput" onChange={(event) => this._onChangeMaterial("cas", event.target.value.toString().toUpperCase())}/>
                             </td>
                             <td>
                                 IFRA RESTRICTED: 
-                                <select onChange={(event) => this._onChangeMaterial("ifra_restricted", event.target.value === "TRUE")}>
+                                <select className="databaseinput" onChange={(event) => this._onChangeMaterial("ifra_restricted", event.target.value === "TRUE")}>
                                     <option value="TRUE">TRUE</option>
                                     <option value="FALSE">FALSE</option>
                                 </select>
