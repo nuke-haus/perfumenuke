5bd8b9000edbd80e2da33a8d3caf4079031de063
nuke-haus <nick.ratuski@gmail.com>
2021-12-20 16:34:48 -0500

    css

--- scripts/InfoButton.jsx
+++ scripts/InfoButton.jsx
@@ -15,30 +15,32 @@ class InfoButton extends React.Component {
     _renderModal() {
         if (this.state.showModal) {
             return (
-                <div className="modal">
-                    <div className="modalrow">
-                        {"SCENT: " + (this.props.material.scent || "N/A")}
+                <div className="modalback">
+                    <div className="modal">
+                        <div className="modalrow">
+                            {"SCENT: " + (this.props.material.scent || "N/A")}
+                        </div>
+                        <div className="modalrow">
+                            {"USAGE: " + (this.props.material.usage || "N/A")}
+                        </div>
+                        <div className="modalrow">
+                            {"NOTE: " + (this.props.material.note || "N/A")}
+                        </div>
+                        <div className="modalrow">
+                            {"LONGEVITY (HOURS): " + (this.props.material.longevity || "N/A")}
+                        </div>
+                        <div className="modalrow">
+                            {"IMPACT: " + (this.props.material.impact || "N/A")}
+                        </div>
+                        <div className="modalrow">
+                            {"CAS NUMBER: " + (this.props.material.cas || "N/A")}
+                        </div>
+                        <button type="button" 
+                                className="modalclose"
+                                onClick={() => this._onClick(false)}>
+                            Close
+                        </button>
                     </div>
-                    <div className="modalrow">
-                        {"USAGE: " + (this.props.material.usage || "N/A")}
-                    </div>
-                    <div className="modalrow">
-                        {"NOTE: " + (this.props.material.note || "N/A")}
-                    </div>
-                    <div className="modalrow">
-                        {"LONGEVITY (HOURS): " + (this.props.material.longevity || "N/A")}
-                    </div>
-                    <div className="modalrow">
-                        {"IMPACT: " + (this.props.material.impact || "N/A")}
-                    </div>
-                    <div className="modalrow">
-                        {"CAS NUMBER: " + (this.props.material.cas || "N/A")}
-                    </div>
-                    <button type="button" 
-                            className="modalclose"
-                            onClick={() => this._onClick(false)}>
-                        Close
-                    </button>
                 </div>
             );
         }
