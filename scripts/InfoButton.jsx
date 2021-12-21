78bd3bfc9a43cf73b5d840bf20617b19c6643051
nuke-haus <nick.ratuski@gmail.com>
2021-12-21 12:58:49 -0500

    fix

--- scripts/InfoButton.jsx
+++ scripts/InfoButton.jsx
@@ -12,29 +12,52 @@ class InfoButton extends React.Component {
         this.setState({showModal: show});
     }
 
+    _renderDiv(text) {
+        return (
+            <div className="modalrow">
+                {text}
+            </div>
+        );
+    }
+
     _renderModal() {
         if (this.state.showModal) {
+            const company = (this.props.material.company != null)
+                ? this._renderDiv("COMPANY: " + this.props.material.company)
+                : null;
+            const scent = (this.props.material.scent != null)
+                ? this._renderDiv("SCENT: " + this.props.material.scent)
+                : null;
+            const usage = (this.props.material.usage != null)
+                ? this._renderDiv("USAGE: " + this.props.material.usage)
+                : null;
+            const note = (this.props.material.note != null)
+                ? this._renderDiv("NOTE: " + this.props.material.note)
+                : null;
+            const longevity = (this.props.material.longevity != null)
+                ? this._renderDiv("LONGEVITY (HOURS): " + this.props.material.longevity)
+                : null;
+            const impact = (this.props.material.impact != null)
+                ? this._renderDiv("IMPACT: " + this.props.material.impact)
+                : null;
+            const dilute = (this.props.material.dilute != null)
+                ? this._renderDiv("RECOMMENDED DILUTION: " + this.props.material.dilute)
+                : null;
+            const cas = (this.props.material.cas != null)
+                ? this._renderDiv("CAS NUMBER: " + this.props.material.cas)
+                : null;
+
             return (
                 <div className="modal">
                     <div className="modalback" onClick={() => this._onClick(false)}/>
-                    <div className="modalrow">
-                        {"SCENT: " + (this.props.material.scent || "N/A")}
-                    </div>
-                    <div className="modalrow">
-                        {"USAGE: " + (this.props.material.usage || "N/A")}
-                    </div>
-                    <div className="modalrow">
-                        {"NOTE: " + (this.props.material.note || "N/A")}
-                    </div>
-                    <div className="modalrow">
-                        {"LONGEVITY (HOURS): " + (this.props.material.longevity || "N/A")}
-                    </div>
-                    <div className="modalrow">
-                        {"IMPACT: " + (this.props.material.impact || "N/A")}
-                    </div>
-                    <div className="modalrow">
-                        {"CAS NUMBER: " + (this.props.material.cas || "N/A")}
-                    </div>
+                    {company}
+                    {scent}
+                    {usage}
+                    {note}
+                    {longevity}
+                    {impact}
+                    {dilute}
+                    {cas}
                 </div>
             );
         }
@@ -48,7 +71,7 @@ class InfoButton extends React.Component {
                     {this.props.material.name || "???"}
                 </span>
                 <span className="modalbutton" onClick={() => this._onClick(true)}>
-                    {"\u{1F4DC}"}
+                    {"\u{1F4C3}"}
                 </span>
                 {this._renderModal()}
             </div>
