6e6d81953966db925f0513e72807b128f4567107
nüke <nuke.twoski@gmail.com>
2023-02-10 23:15:54 -0500

    Avoid unnecessary setState calls

--- scripts/FormulaBody.jsx
+++ scripts/FormulaBody.jsx
@@ -56,8 +56,8 @@ class FormulaBody extends React.Component {
         PN.database.activeFormula[key] = value;
         if (recompute) {
             PN.recomputeFormula();
+            this.setState({formulaButtonKey: PN.guid(), detailsKey: PN.guid()});
         }
-        this.setState({formulaButtonKey: PN.guid(), detailsKey: PN.guid()});
     }
 
     _changeIngredient(id, ingredient) {
